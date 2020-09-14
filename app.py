9ac9ec7ffa8b61823e096d7183ee07833ae1f656
XeyyamSherif <you@example.com>
2020-09-14 14:55:15 +0400

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+start = False
+stop = False
+
+while True:
+    command = input("commands: 'start' , 'stop '  >").lower()
+    if command == "start":
+        if start == False:
+            start = True
+            stop = False
+            print("car started")
+        else:
+            print("you cant start,car already started")
+    elif command == "stop":
+        if stop == False:
+            stop = True
+            start = False
+            print("car stopped")
+        else :
+            print("you cant stop,car already stoppped")
